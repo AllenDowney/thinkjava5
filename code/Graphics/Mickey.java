f73746837870c51914885e77a7b8f1ec41110c0f
allendowney <allendowney@2eafce11-4d17-484a-7b09-0676bd32ee6e>
2011-07-25 19:35:10 +0000

    Adding CardTable.java

--- code/Graphics/Mickey.java
+++ code/Graphics/Mickey.java
@@ -7,41 +7,52 @@
  * @author Allen B. Downey
  */
 
+import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import javax.swing.JFrame;
 
-public class Mickey extends Frame {
 
-    public static void main(String[] args) {
-	Mickey gd = new Mickey();
-	gd.setSize(400, 400);
-	gd.setBackground(Color.white);
-	gd.setVisible(true);
-    }
+public class Mickey extends Canvas {
 
-    public void fillOval(Graphics g, Rectangle bb) {
+    public void boxOval(Graphics g, Rectangle bb) {
 	g.fillOval(bb.x, bb.y, bb.width, bb.height);
     }
 
     public void mickey(Graphics g, Rectangle bb) {
-	fillOval(g, bb);
+	boxOval(g, bb);
 
 	int dx = bb.width/2;
 	int dy = bb.height/2;
 	Rectangle half = new Rectangle(bb.x, bb.y, dx, dy);
 
 	half.translate(-dx/2, -dx/2);
-	fillOval(g, half);
+	boxOval(g, half);
 
 	half.translate(dx*2, 0);
-	fillOval(g, half);
+	boxOval(g, half);
     }
 
     public void paint(Graphics g) {
 	Rectangle bb = new Rectangle(100, 150, 200, 200);
 	g.setColor(Color.gray);
 	mickey(g, bb);
+    }
+
+    public static void main(String[] args) {
+	// make the frame
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	// add the canvas
+	Canvas canvas = new Mickey();
+        canvas.setSize(400, 400);
+	canvas.setBackground(Color.white);
+        frame.getContentPane().add(canvas);
+
+	// show the frame
+	frame.pack();
+        frame.setVisible(true);
     }
 }
--- code/Graphics/MickeySoln.java
+++ code/Graphics/MickeySoln.java
@@ -7,57 +7,55 @@
  * @author Allen B. Downey
  */
 
+import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import javax.swing.JFrame;
 
-public class MickeySoln extends Frame {
 
-    public static void main(String[] args) {
-	MickeySoln gd = new MickeySoln();
-	gd.setSize(400, 400);
-	gd.setBackground(Color.white);
-	gd.setVisible(true);
-    }
+public class MickeySoln extends Canvas {
 
-    public void fillOval(Graphics g, Rectangle bb) {
+    public void boxOval(Graphics g, Rectangle bb) {
 	g.fillOval(bb.x, bb.y, bb.width, bb.height);
     }
 
     public void mickey(Graphics g, Rectangle bb) {
-	fillOval(g, bb);
-
-	int dx = bb.width/2;
-	int dy = bb.height/2;
-	Rectangle half = new Rectangle(bb.x, bb.y, dx, dy);
-
-	half.translate(-dx/2, -dx/2);
-	fillOval(g, half);
-
-	half.translate(dx*2, 0);
-	fillOval(g, half);
-    }
-
-    public void recurse(Graphics g, Rectangle bb) {
-	fillOval(g, bb);
+	boxOval(g, bb);
 	if (bb.width < 3) {
 	    return;
 	}
+
 	int dx = bb.width/2;
 	int dy = bb.height/2;
 	Rectangle half = new Rectangle(bb.x, bb.y, dx, dy);
 
 	half.translate(-dx/2, -dx/2);
-	recurse(g, half);
+	mickey(g, half);
 
 	half.translate(dx*2, 0);
-	recurse(g, half);
+	mickey(g, half);
     }
 
     public void paint(Graphics g) {
 	Rectangle bb = new Rectangle(100, 150, 200, 200);
 	g.setColor(Color.gray);
-	recurse(g, bb);
+	mickey(g, bb);
+    }
+
+    public static void main(String[] args) {
+	// make the frame
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	// add the canvas
+	Canvas canvas = new MickeySoln();
+        canvas.setSize(400, 400);
+	canvas.setBackground(Color.white);
+        frame.getContentPane().add(canvas);
+
+	// show the frame
+	frame.pack();
+        frame.setVisible(true);
     }
 }
