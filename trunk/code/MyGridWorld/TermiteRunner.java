f401f0c13fe3501970aeae19018a639695901eb7
mayfiecs <mayfiecs@2eafce11-4d17-484a-7b09-0676bd32ee6e>
2012-12-19 17:03:15 +0000

    Adding minor corrections from Leslie; version 5.1.2

--- trunk/code/MyGridWorld/TermiteRunner.java
+++ trunk/code/MyGridWorld/TermiteRunner.java
@@ -25,31 +25,24 @@ import java.awt.Color;
  * This class runs a world that contains Termites. <br />
  */
 public class TermiteRunner {
-
+    
     public static void main(String[] args) {
-	ActorWorld world = new ActorWorld();
-	makeFlowers(world, 20);
-
+        ActorWorld world = new ActorWorld();
+        makeFlowers(world, 20);
+        
         Termite alice = new Termite();
-	world.add(alice);
-
+        world.add(alice);
+        
         Termite bob = new Termite();
-	bob.setColor(Color.blue);
-	world.add(bob);
-
+        bob.setColor(Color.blue);
+        world.add(bob);
+        
         world.show();
     }
-
+    
     public static void makeFlowers(ActorWorld world, int n) {
-	for (int i=0; i<n; i++) {
-	    world.add(new EternalFlower());
-	}
+        for (int i=0; i<n; i++) {
+            world.add(new EternalFlower());
+        }
     }
-}
-
-/**
- * A flower that doesn't age.
- */
-class EternalFlower extends Flower {
-    public void act() {}
 }
